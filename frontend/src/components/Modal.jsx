65a146588a4712b0a922a72ab25dc42c393071d3
Abhi <meabhishek1512@gmail.com>
2025-05-13 06:55:24 +0530

    added modal layout

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,7 +1,45 @@
-import React from "react"
+const Modal = ({ children, isOpen, onClose, hideHeader, title }) => {
+  return (
+    <div className="fixed inset-0 z-50 flex justify-center items-center w-full h-full bg-black/40">
+      {/* Modal Content */}
+      <div className="relative flex flex-col w-full max-w-md bg-white rounded-lg shadow-lg overflow-hidden">
+        {/* Close Button */}
+        <button
+          className="text-gray-400 bg-transparent hover:bg-orange-100 hover:text-gray-900 rounded-lg text-sm w-8 h-8 flex justify-center items-center absolute top-3.5 right-3.5"
+          type="button"
+          onClick={onClose}
+        >
+          <svg
+            className="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1l6 6m0 0l6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+        </button>
 
-const Modal = () => {
-  return <div>Modal</div>
+        {/* Modal Header */}
+        {!hideHeader && (
+          <div className="flex items-center justify-between p-4 border-b border-gray-200">
+            <h3 className="md:text-lg font-medium text-gray-900">{title}</h3>
+          </div>
+        )}
+
+        {/* Modal Body */}
+        <div className="p-4 overflow-y-auto custom-scrollbar max-h-[80vh]">
+          {children}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Modal
