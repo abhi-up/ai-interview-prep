df89ec519b76d7e4e8644067a35a829a6534e8d8
Abhi <meabhishek1512@gmail.com>
2025-05-13 23:44:51 +0530

    fixed the login container and added input box

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import Input from "../../components/Inputs/Input"
 
 const Login = () => {
   const [email, setEmail] = useState("")
@@ -8,19 +9,18 @@ const Login = () => {
 
   const navigate = useNavigate()
 
-  // handle login form submit
   const handleLogin = async (e) => {
     e.preventDefault()
   }
 
   return (
-    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+    <div className="w-full max-w-md p-7 flex flex-col justify-center bg-white  rounded-xl">
       <h3 className="text-lg font-semibold text-black">Welcome Back</h3>
       <p className="text-xs text-slate-700 mt-[5px] mb-6">
         Please enter your details to log in
       </p>
 
-      <form onSubmit={handleLogin}>
+      <form onSubmit={handleLogin} className="space-y-4">
         <Input
           value={email}
           onChange={({ target }) => setEmail(target.value)}
