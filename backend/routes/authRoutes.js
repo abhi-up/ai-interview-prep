d028b2d203291cb86550e627e9c9438cc0f43b6e
Abhi <meabhishek1512@gmail.com>
2025-05-19 17:05:44 +0530

    added upload route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,6 +5,7 @@ const {
   getUserProfile,
 } = require("../controllers/authController")
 const { protect } = require("../middlewares/authMiddleware")
+const upload = require("../middlewares/uploadMiddleware")
 
 const router = express.Router()
 
@@ -12,5 +13,16 @@ const router = express.Router()
 router.post("/register", registerUser)
 router.post("/login", loginUser)
 router.get("/profile", protect, getUserProfile)
+
+// Image Upload
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" })
+  }
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+  }`
+  res.status(200).json({ imageUrl })
+})
 
 module.exports = router
