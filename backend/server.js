9ed450585b306447a211535ffe68936ce6e9e9fa
Abhishek <meabhishek1512@gmail.com>
2025-07-14 19:55:14 +0530

    generate interview questions using ai endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
-const path = require("path")
-const connectDB = require("./config/db")
-const authRoutes = require("./routes/authRoutes")
-const sessionRoutes = require("./routes/sessionRoutes")
-const questionRoutes = require("./routes/questionRoutes")
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const sessionRoutes = require("./routes/sessionRoutes");
+const questionRoutes = require("./routes/questionRoutes");
+const { protect } = require("./middlewares/authMiddleware");
+const {
+  generateInterviewQuestions,
+  generateConceptExplanation,
+} = require("./controllers/aiController");
 
-const app = express()
+const app = express();
 
 // Middleware to handle CORS
 app.use(
@@ -16,27 +21,27 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
-)
+);
 
 // Database connection
-connectDB()
+connectDB();
 
 // Middleware
-app.use(express.json())
+app.use(express.json());
 
 // Routes
-app.use("/api/auth", authRoutes)
-app.use("/api/sessions", sessionRoutes)
-app.use("/api/questions", questionRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/sessions", sessionRoutes);
+app.use("/api/questions", questionRoutes);
 
-// app.use("/api/ai/generate-questions", protect, generateInterviewQuestions)
-// app.use("/api/ai/generate-explanation", protect, generateConceptExplanation)
+app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
 // Serve upload folder
-app.use("/uploads", express.static(path.join(__dirname, "uploads")))
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Start Server
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
